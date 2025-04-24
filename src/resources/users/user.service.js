1131d080c4b8d6e3a9bfacb0bc123097192fc6cf
CocaineJimmy <120776686+CocaineJimmy@users.noreply.github.com>
2025-04-24 19:23:53 +0300

    user service fix

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,21 @@
 import * as usersRepo from './user.memory.repository.js';
 
 const getAll = () => usersRepo.getAll();
+// resources/users/user.service.js
+
+const userRepo = require('./user.memory.repository');
+
+const getAll = async () => await userRepo.getAll();
+
+const getById = async (id) => await userRepo.getById(id);
+
+const create = async (data) => await userRepo.create(data);
+
+const update = async (id, data) => await userRepo.update(id, data);
+
+const remove = async (id) => await userRepo.remove(id);
+
+module.exports = { getAll, getById, create, update, remove };
+
 
 export { getAll };
