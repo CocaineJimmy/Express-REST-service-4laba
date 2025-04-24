f10718848d417b89206d0bb62f773ad963fc00fc
CocaineJimmy <120776686+CocaineJimmy@users.noreply.github.com>
2025-04-24 19:22:28 +0300

    server fix 2.0

--- src/server.js
+++ src/server.js
@@ -1,22 +1,20 @@
 // server.js
 const express = require('express');
 const config = require('./config');
-const userRouter = require('./user.router');
+const userRouter = require('./resources/users/user.router');
 
 const app = express();
 
-// JSON‑парсер для входящих запросов
 app.use(express.json());
 
-// Регистрируем маршруты для ресурса пользователей
+// Регистрируем маршруты для ресурса users
 app.use('/users', userRouter);
 
-// Обработка запросов к несуществующим эндпоинтам
+// Обработка несуществующих маршрутов
 app.use((req, res) => {
   res.status(404).json({ message: 'Not Found' });
 });
 
-// Запуск сервера на порту, указанном в config.js
 app.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
 });
